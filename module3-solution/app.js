8b7ea8ba4b7fe7f61de56ffe0805858febc154f4
Julien <julien1982@hotmail.com>
2017-04-15 02:40:55 +0200

    final version with directive

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -5,7 +5,8 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.constant('ApiMenuItemsUrl', "https://davids-restaurant.herokuapp.com/menu_items.json");
+.constant('ApiMenuItemsUrl', "https://davids-restaurant.herokuapp.com/menu_items.json")
+.directive('foundItems', FoundItems);
 
 //////////////////////////////////////////
 // NarrowItDownController configuration //
@@ -17,10 +18,21 @@ function NarrowItDownController(MenuSearchService) {
   controller.searchTerm = "";
 
   controller.getMatchedMenuItems = function () {
-    MenuSearchService.getMatchedMenuItems(controller.searchTerm).then(function (data) {
-      controller.found = data;
-      console.log("controller list : ", controller.found);
-    });
+    // MenuSearchService method return a promise,
+    // found array will be updated when the result will be available in then() function
+    if(controller.searchTerm) {
+      MenuSearchService.getMatchedMenuItems(controller.searchTerm).then(function (data) {
+        controller.found = data;
+      });
+    }
+    else {
+      controller.found = [];
+    }
+
+  };
+
+  controller.removeMenuItem = function (itemIndex) {
+    controller.found.splice(itemIndex, 1);
   };
 }
 
@@ -49,10 +61,27 @@ function MenuSearchService($http, ApiMenuItemsUrl) {
     return items.filter(filterItems, searchTerm);
   }
 
-  // keep the item if description contains searchTerm
+  // keep the item if description contains the search term
   function filterItems(item) {
-    return (item.description !== undefined && item.description.includes(this));
+    // search is not case sensitive
+    return  (item.description !== undefined && item.description.toLowerCase().includes(this.toLowerCase()));
   }
+}
+
+//////////////////////////////////////////
+// FoundItems directive configuration   //
+//////////////////////////////////////////
+function FoundItems() {
+  var ddo = {
+    templateUrl: 'menuItemsList.html',
+    restrict: 'E',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    }
+  };
+
+  return ddo;
 }
 
 })();
